5479cc2f19364ecfb44eaf3990b7cc81eaadd43e
David Rifkin <davidlrifkin@gmail.com>
2015-04-23 17:34:43 -0400

    Create basic newNum generator method

--- js/game.js
+++ js/game.js
@@ -18,6 +18,17 @@ Game.prototype.newBoard = function(){
 	return this.board;
 }
 
+Game.prototype.newNum = function(){
+	var spot = Math.floor((Math.random()))
+	var arrBoard = this.board.split("")
+
+	if (arrBoard[spot] === "0") {
+		arrBoard[spot] = "2"
+		this.board=  arrBoard.join("")
+		console.log(this.board)
+	}
+}
+
 Game.prototype.loop = function(){
 	console.log(game.toString());
 }
